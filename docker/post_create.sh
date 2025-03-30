6217c883bb77e0e52953bf2252fb00f35a4a8577
Jesse Buhagiar <jooster669@gmail.com>
2025-03-30 22:08:25 +1100

    chore: Add Docerfile and `.devcontainer`

---
+++ docker/post_create.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+echo "export N64_LIBGCCDIR=/opt/crashsdk/lib/gcc/mips64-elf/12.2.0" >> ~/.bashrc
+echo "export PATH=$PATH:/opt/crashsdk/bin" >> ~/.bashrc
+
+echo "deb [trusted=yes] https://crashoveride95.github.io/apt/ ./" | sudo tee /etc/apt/sources.list.d/n64sdk.list
+
+sudo apt update
+sudo apt install -y binutils-mips-n64 gcc-mips-n64 newlib-mips-n64
+sudo apt install -y n64sdk
+
+sudo dpkg --add-architecture i386
+sudo apt install -y root-compatibility-environment
+
+echo "export ROOT=/etc/n64" >> ~/.bashrc
+
+sudo apt install -y n64-demos
