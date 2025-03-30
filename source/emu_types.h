54b729e8ed3c4fe8e1b8325b4fc2046950f52649
Jesse Buhagiar <jooster669@gmail.com>
2025-03-30 22:45:59 +1100

    feat: Add common emulator types header

---
+++ source/emu_types.h
@@ -0,0 +1,27 @@
+/**
+ * @file emu_types.h
+ *
+ * @brief Emulator Types
+ *
+ * @author Jesse Buhagiar (jooster669@gmail.com)
+ *
+ * SPDX-License-Identifier: GNU General Public License v3.0 only
+ */
+
+#pragma once
+
+#include <cstdint>
+
+//!
+//! @addtogroup emu_types Emulator Typedefs
+//! @{
+//!
+
+typedef std::uint8_t  u8;   /**< uint8_t alias typedef */
+typedef std::uint16_t u16;  /**< uint16_t alias typedef */
+typedef std::uint32_t u32;  /**< uint32_t alias typedef */
+typedef std::uint64_t u64;  /**< uint64_t alias typedef */
+
+//!
+//! @}
+//!
