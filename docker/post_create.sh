afc8e0e7b1636170bfe9eb7e240fd77718e9d2f4
Jesse Buhagiar <jooster669@gmail.com>
2025-04-21 00:36:21 +1000

    fix: Install `cmake` in `post_create.sh`

--- docker/post_create.sh
+++ docker/post_create.sh
@@ -6,7 +6,7 @@ echo "export PATH=$PATH:/opt/crashsdk/bin" >> ~/.bashrc
 echo "deb [trusted=yes] https://crashoveride95.github.io/apt/ ./" | sudo tee /etc/apt/sources.list.d/n64sdk.list
 
 sudo apt update
-sudo apt install -y binutils-mips-n64 gcc-mips-n64 newlib-mips-n64
+sudo apt install -y binutils-mips-n64 gcc-mips-n64 newlib-mips-n64 cmake
 sudo apt install -y n64sdk
 
 sudo dpkg --add-architecture i386
